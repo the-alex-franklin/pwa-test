4eadc92414c86df9e21f3a13e3a2044be03dc0e5
Alex <the.alex.franklin@gmail.com>
2025-02-14 14:43:45 -0800

    trying again

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,4 @@ import "./style.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 
-if ("serviceWorker" in navigator) {
-  addEventListener("load", () => {
-    navigator.serviceWorker.register("/sw.js").then((registration) => {
-      console.log("Service Worker registered with scope:", registration.scope);
-    }).catch((error) => {
-      console.error("Service Worker registration failed:", error);
-    });
-  });
-}
-
 createApp(App).mount("#app");
