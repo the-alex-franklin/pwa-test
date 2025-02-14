de234d9fc8c55e197fee56659e49c2cfe24fa73b
Alex <the.alex.franklin@gmail.com>
2025-02-14 14:51:19 -0800

    icons

--- vite.config.ts
+++ vite.config.ts
@@ -19,12 +19,12 @@ export default defineConfig({
         theme_color: "#42b983",
         icons: [
           {
-            src: "/icons/icon-192x192.png",
+            src: "/icons/web-app-manifest-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/icons/icon-512x512.png",
+            src: "/icons/web-app-manifest-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
